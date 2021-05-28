0b7aefa729743fdb7bfc5c79f76bf84af913cf5d
Evan Greco <e.greco12@gmail.com>
2021-05-28 17:12:44 -0500

    1 HP lol

--- GolfBall.gd
+++ GolfBall.gd
@@ -8,6 +8,7 @@ onready var ShotPowerLine = $ShotPowerLine
 onready var CollisionShapeBall = $CollisionShapeBall
 
 var Charging: bool = false
+var BallHit: bool = false
 var InitialClickPos: Vector2
 
 # Called when the node enters the scene tree for the first time.
@@ -15,22 +16,24 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	#if charging:
-	if !Charging and (Input.get_mouse_button_mask() & BUTTON_MASK_LEFT):
-		Charging = true
-		InitialClickPos = get_viewport().get_mouse_position()
-	
-	if Charging:
-		var chargeVector = InitialClickPos - get_viewport().get_mouse_position()
-		ShotPowerLine.points[1] = chargeVector
+	# Only allowed to hit the ball once, 1 HP lol
+	if !BallHit:
+		if !Charging and (Input.get_mouse_button_mask() & BUTTON_MASK_LEFT):
+			Charging = true
+			InitialClickPos = get_viewport().get_mouse_position()
 		
-		if !(Input.get_mouse_button_mask() & BUTTON_MASK_LEFT):
-			Charging = false
-			ShotPowerLine.points[1] = Vector2()
+		if Charging:
+			var chargeVector = InitialClickPos - get_viewport().get_mouse_position()
+			ShotPowerLine.points[1] = chargeVector
 			
-			self.apply_central_impulse(chargeVector*-1)
-			
-		 #var distance = CollisionShapeBall.position.distance_to(get_viewport().get_local_mouse_position())
+			if !(Input.get_mouse_button_mask() & BUTTON_MASK_LEFT):
+				Charging = false
+				ShotPowerLine.points[1] = Vector2()
+				
+				self.apply_central_impulse(chargeVector*-1)
+				BallHit = true
+				
+			 #var distance = CollisionShapeBall.position.distance_to(get_viewport().get_local_mouse_position())
 		
 		
 
