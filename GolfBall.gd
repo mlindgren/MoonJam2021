829d7d6c5f4bd14ec175f48a292abd2cd7e9a7f2
Mitch Lindgren <mitch@lindg.ren>
2021-05-28 13:57:45 -0700

    Added line

--- GolfBall.gd
+++ GolfBall.gd
@@ -4,14 +4,30 @@ extends RigidBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+onready var ShotPowerLine = $ShotPowerLine
+onready var CollisionShapeBall = $CollisionShapeBall
 
+var charging: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _process(delta):
+	#if charging:
+	ShotPowerLine.points[1] = self.to_local(get_viewport().get_mouse_position())
+		 #var distance = CollisionShapeBall.position.distance_to(get_viewport().get_local_mouse_position())
+		
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+
+#func _on_GolfBall_input_event(viewport, event, shape_idx):
+#	print("Input event")
+#	if event is InputEventMouseButton:
+#		print("Input event at ", event.position)
+#		ShotPowerLine.points[1] = event.position
+#		ShotPowerLine.update()
