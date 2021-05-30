9e3047f1d7a04b043c4c502502da045dd3311a64
Evan Greco <e.greco12@gmail.com>
2021-05-29 21:30:30 -0500

    Add popup on game finished

--- UI/UIScript.gd
+++ UI/UIScript.gd
@@ -5,6 +5,7 @@ extends Control
 # var a = 2
 # var b = "text"
 onready var _popup : Popup = $CanvasLayer/HoleFinishedPopup
+onready var _winnerPopup : Popup = $CanvasLayer/WinnerPopup
 onready var _camera : Camera2D = $"/root/Course/GolfBall/BallCam"
 
 # Called when the node enters the scene tree for the first time.
@@ -18,9 +19,7 @@ func _process(delta):
 
 func _on_NextHoleButton_pressed():
 	Global.ballInHole = false
-	if Global.currHole < Global.MAX_HOLE:
-		Global.currHole += 1
-		Global.hitPoints += 1
+	if Global.currHole <= Global.MAX_HOLE:
 		print("Changing to hole: " + String(Global.currHole))
 		get_tree().change_scene("res://Courses/Hole" + String(Global.currHole) + ".tscn")
 	else:
@@ -29,4 +28,9 @@ func _on_NextHoleButton_pressed():
 func _on_GolfHole_body_entered(body):
 	print("HOLE ENTERED LOL")
 	Global.ballInHole = true
-	_popup.popup_centered()
+	if Global.currHole < Global.MAX_HOLE:
+		Global.currHole += 1
+		Global.hitPoints += 1
+		_popup.popup_centered()
+	else:
+		_winnerPopup.popup_centered()
