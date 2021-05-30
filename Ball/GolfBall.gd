7169becaa213b88b0a2faebbc302e6675bcf4fa4
Mitch Lindgren <mitch@lindg.ren>
2021-05-30 14:31:29 -0700

    Added ball stopping code

--- Ball/GolfBall.gd
+++ Ball/GolfBall.gd
@@ -1,3 +1,4 @@
+class_name GolfBall
 extends RigidBody2D
 
 signal ball_hit
@@ -20,6 +21,15 @@ const HOLE_ENTRY_DURATION : float = 0.25
 
 var charging: bool = false
 
+# Counts how many slopes the ball has entered. I initially used a bool for this
+# to track whether the ball was on a slope, but when two slope areas are directly
+# adjacent, the ball can enter a second slope before it exits the first one, and
+# then when the first one is exited, the bool would be set to false even though
+# the ball is still on a slope. So instead we count how many slopes we've entered
+# and if it's >0 we're on a slope. I don't think signals are multithreaded so
+# this should work fine.
+var slopeCount: int = 0
+
 var _chargeVector: Vector2
 var _ballHit: bool = false
 var _ballInMotion: bool = false
@@ -30,29 +40,31 @@ func _ready():
 	pass # Replace with function body.
 
 func _isBallMoving():
-	return _ballHit && self.linear_velocity.length() > MIN_VELOCITY
+	return _ballHit && linear_velocity.length() > MIN_VELOCITY
 		
 func _physics_process(_delta):
-	_ballInMotion = self._isBallMoving()
+		
+	if slopeCount == 0 and linear_velocity.length() < MIN_VELOCITY:
+		linear_velocity = Vector2()
+		
+	_ballInMotion = _isBallMoving()
 	
-	# _sprite.material.set_shader_param("Direction", self.linear_velocity.normalized())
-	# _sprite.material.set_shader_param("Speed", self.linear_velocity.length())
 	_sprite.material.set_shader_param(
 		"Position",
-		self.position / _sprite.region_rect.size * -1);
+		position / _sprite.region_rect.size * -1);
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if charging:
 		_chargeVector =  (_initialClickPos - get_viewport().get_mouse_position()).clamped(
 			MAX_CHARGE_VECTOR_LENGTH)
-		_shotPowerLine.points[1] = _chargeVector.rotated(self.rotation * -1)
+		_shotPowerLine.points[1] = _chargeVector.rotated(rotation * -1)
 		
 		if !(Input.get_mouse_button_mask() & BUTTON_MASK_LEFT):
 			charging = false
 			_shotPowerLine.points[1] = Vector2()
 			
-			self.apply_central_impulse(_chargeVector * POWER_SCALING_FACTOR)
+			apply_central_impulse(_chargeVector * POWER_SCALING_FACTOR)
 			_ballHit = true
 			Global.score += 1
 			emit_signal("ball_hit")
@@ -74,12 +86,12 @@ func _on_GolfBall_body_entered(body):
 		emit_signal("ball_died")
 
 func _on_GolfHole_body_entered(body):
-	print("Hole entered at ", self.linear_velocity.length())
-	if self.linear_velocity.length() < MAX_HOLE_ENTRY_VELOCITY:
+	print("Hole entered at velocity ", linear_velocity.length())
+	if linear_velocity.length() < MAX_HOLE_ENTRY_VELOCITY:
 		_tween.interpolate_property(
 			self,
 			"scale",
-			self.scale,
+			scale,
 			Vector2(),
 			HOLE_ENTRY_DURATION,
 			Tween.TRANS_LINEAR,
--- CourseElements/LinearSlope.gd
+++ CourseElements/LinearSlope.gd
@@ -3,3 +3,13 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.gravity_vec = self.gravity_vec.rotated(self.rotation)
+
+func _on_SlopeArea_body_entered(body):
+	if body is GolfBall:
+		body.slopeCount += 1
+		print("Slope entered")
+
+func _on_SlopeArea_body_exited(body):
+	if body is GolfBall:
+		body.slopeCount -= 1
+		print("Slope exited")
