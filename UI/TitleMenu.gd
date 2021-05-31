4f1bbfcacbbdc1a2b3b13da840a188083079413f
Mitch Lindgren <mitch@lindg.ren>
2021-05-30 23:40:40 -0700

    More updates to title screen

--- UI/TitleMenu.gd
+++ UI/TitleMenu.gd
@@ -5,11 +5,24 @@ const FIRST_COURSE_PATH = "res://Courses/Hole1.tscn"
 var selected_menu = 0
 var preload_course : Resource
 
+onready var title = $Title
+
+var time = 0
+
 func _ready():
 	preload_course = preload(FIRST_COURSE_PATH)
 
-func _process(_delta):
+func _process(delta):
 	rect_scale =  get_viewport().size / rect_size
+	
+	time += delta
+	var scale_multiplier = 1.0 + sin(time) * 0.2
+	title.rect_scale = Vector2(1, 1) * scale_multiplier
 
-func _on_TextureButton_pressed():
+func _on_GodGamerModeButton_pressed():
+	Global.game_mode = Global.GAME_MODE.GOD_GAMER
+	get_tree().change_scene(FIRST_COURSE_PATH)
+
+func _on_HardstuckPlatButton_pressed():
+	Global.game_mode = Global.GAME_MODE.HARDSTUCK_PLAT
 	get_tree().change_scene(FIRST_COURSE_PATH)
