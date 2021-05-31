8720681928174ac7e03f9d8b755690cb6c46dd38
Mitch Lindgren <mitch@lindg.ren>
2021-05-30 20:24:59 -0700

    Added title screen art

--- Global.gd
+++ Global.gd
@@ -1,7 +1,11 @@
 extends Node
 
+enum GAME_MODE {GOD_GAMER, HARDSTUCK_PLAT}
+
 const MAX_HOLE = 10 # needs to be n + 1
 var currHole = 1
 var ballInHole = false
 var hitPoints = 3
 var score = 0
+
+var game_mode = GAME_MODE.HARDSTUCK_PLAT
