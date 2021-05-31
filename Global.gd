55c0bed13eb6761f3d8c8aed57d26a4ebb357dab
Evan Greco <e.greco12@gmail.com>
2021-05-30 22:42:55 -0500

    fix global

--- Global.gd
+++ Global.gd
@@ -1,13 +1,7 @@
 extends Node
 
-<<<<<<< HEAD
 const MAX_HOLE = 16 # needs to be n + 1
-=======
 enum GAME_MODE {GOD_GAMER, HARDSTUCK_PLAT}
-
-const MAX_HOLE = 15 # needs to be n + 1
-
->>>>>>> ec45ba7c0917261d3b42f1f3b74d5d6083175ec3
 var currHole = 1
 var ballInHole = false
 var hitPoints = 3
